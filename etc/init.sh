f9c953127a04c17344daf61d583abfa22116a879
stas <andreeff.stas2014@gmail.com>
2022-11-02 09:27:56 +0300

    setting gunicorn

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 -c /home/box/web/hello.py hello:process_http_request
+
